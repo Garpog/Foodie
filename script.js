f1ef86d2c0e2502928ac473bc4a18ad5f5e9c128
LoJeff <32105587+LoJeff@users.noreply.github.com>
2018-01-05 13:26:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 "use strict";
 function displayFridge() {
-  $("#fridge").css("display", "block
+  $("#fridge").css("display", "block");
   $("#ingredients").css("display", "none");
   $("#recipes").css("display", "none");
 };
 
 function displayIngr() {
   $("#fridge").css("display", "none");
-  $("#ingredients").css("display", "block
+  $("#ingredients").css("display", "block");
   $("#recipes").css("display", "none");
 };
 
 function displayRecipes() {
   $("#fridge").css("display", "none");
   $("#ingredients").css("display", "none");
-  $("#recipes").css("display", "block
+  $("#recipes").css("display", "block");
 };
